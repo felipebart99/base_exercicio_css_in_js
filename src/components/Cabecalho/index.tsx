6e17823d98bc15ea5ed939ffb92d4ad50ecc4eb8
felipe bartolomeu <felipebart1999@gmail.com>
2025-02-02 16:13:45 -0300

    finalizei

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,30 @@
-import styles from './Cabecalho.module.css'
+// import styles from './Cabecalho.module.css'
+
+// const Cabecalho = () => (
+//   <header className={styles.cabecalho}>
+//     <h1>EBAC Jobs</h1>
+//   </header>
+// )
+
+// export default Cabecalho
+
+import styled from 'styled-components'
+
+const StyledCabecalho = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
+const Titulo = styled.h1`
+  margin: 0;
+`
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
+  <StyledCabecalho>
+    <Titulo>EBAC Jobs</Titulo>
+  </StyledCabecalho>
 )
 
 export default Cabecalho
--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,62 @@
-import { FormEvent, useState } from 'react'
+// import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+// import styles from './FormVagas.module.css'
+
+// type Props = {
+//   aoPesquisar: (termo: string) => void
+// }
+
+// const FormVagas = ({ aoPesquisar }: Props) => {
+//   const [termo, setTermo] = useState<string>('')
+
+//   const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault()
+//     aoPesquisar(termo.toLocaleLowerCase())
+//   }
+
+//   return (
+//     <form className={styles.form} onSubmit={aoEnviarForm}>
+//       <input
+//         className={styles.campo}
+//         placeholder="Front-end, fullstack, node, design"
+//         onChange={(e) => setTermo(e.target.value)}
+//         type="search"
+//       />
+//       <button className={styles.btnPesquisar} type="submit">
+//         Pesquisar
+//       </button>
+//     </form>
+//   )
+// }
+// export default FormVagas
+
+//formVagas.module.css
+// .form {
+//   display: grid;
+//   grid-template-columns: 1fr auto;
+//   background-color: var(--cor-secundaria);
+//   padding: 32px;
+//   border-radius: 12px;
+//   margin-top: 40px;
+// }
+
+// .btnPesquisar {
+//   background-color: var(--cor-principal);
+//   border: 1px solid var(--cor-principal);
+//   height: 40px;
+//   padding: 0 16px;
+//   font-size: 18px;
+//   color: var(--cor-secundaria);
+//   margin-left: 8px;
+//   cursor: pointer;
+// }
+
+// .campo {
+//   padding: 0 16px;
+//   outline-color: var(--cor-principal);
+// }
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { Form, Botao, Campo } from './FormVagas.module'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +71,20 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Form as="form" onSubmit={aoEnviarForm}>
+      <Campo
+        as="input"
         placeholder="Front-end, fullstack, node, design"
-        onChange={(e) => setTermo(e.target.value)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setTermo(e.target.value)
+        }
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
+      <Botao as="button" type="submit">
         Pesquisar
-      </button>
-    </form>
+      </Botao>
+    </Form>
   )
 }
+
 export default FormVagas
